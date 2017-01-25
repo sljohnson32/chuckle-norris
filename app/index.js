a2efb3ffdbe29c173078440c4f1bc368ce87730a
sljohnson32 <sljohnson32@gmail.com>
2017-01-24 22:41:06 -0700

    add home

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App/App';
+import Home from './components/Home';
 import Jokes from './components/Jokes/Jokes';
 import Settings from './components/Settings/Settings';
 import Favs from './components/Favs/Favs';
@@ -10,7 +11,8 @@ import { Router, Route, Link, IndexRoute, hashHistory } from 'react-router';
 render(
   <Router history={hashHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Jokes} />
+      <IndexRoute component={Home} />
+      <Route path="jokes" component={Jokes} />
       <Route path="favorites" component={Favs} />
       <Route path="settings" component={Settings} />
     </Route>
